341839be2af7ae35c1da7ad00b4192735587e4b7
Jake Hofmann <Jfshofmann@gmail.com>
2023-07-03 16:44:56 -0400

    Optimize for mobile, style text

---
+++ assets/acupuncture.js
@@ -0,0 +1,15 @@
+// const mediaQuery = window.matchMedia('(max-width: 480px)')
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     if (mediaQuery.matches) {
+//         let danapic = document.querySelector(".dana_picture");
+//         let danainfo = document.querySelector(".dana_info");
+
+//         console.log(danainfo);
+//         console.log(danapic);
+
+//         danapic.before(danainfo);
+//     }
+// })
+
+
